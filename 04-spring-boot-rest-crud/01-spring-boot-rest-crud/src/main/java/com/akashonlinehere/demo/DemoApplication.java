a06d7860dd1c9b15d8485824f95f4bab64d761c9
Akash Shridharan <akashshridharan2802@gmail.com>
2024-10-17 13:26:24 +0530

    restcontroller created

---
+++ 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/akashonlinehere/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.akashonlinehere.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
