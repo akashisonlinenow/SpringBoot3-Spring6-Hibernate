dca40819403ddaf14da861c7f147406bb0c2dc3d
Akash Shridharan <akashshridharan2802@gmail.com>
2024-10-23 10:49:26 +0530

    spring security

---
+++ 05-spring-boot-rest-security/00-spring-boot-rest-security-employee-starter-code/src/main/java/com/akashonlinehere/springboot/cruddemo/dao/EmployeeRepository.java
@@ -0,0 +1,8 @@
+package com.akashonlinehere.springboot.cruddemo.dao;
+
+import com.akashonlinehere.springboot.cruddemo.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+    // that's it... no need to write any implementation class
+}
