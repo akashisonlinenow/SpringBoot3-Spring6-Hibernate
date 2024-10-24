56e81a4a9d446827dd6d35d43c7ff0e6b08d1a6c
Akash Shridharan <akashshridharan2802@gmail.com>
2024-10-24 21:06:27 +0530

    data binding - text fields

--- 06-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/akashonlinehere/springboot/thymeleafdemo/controller/HelloWorldController.java
+++ 06-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/akashonlinehere/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -3,14 +3,17 @@ package com.akashonlinehere.springboot.thymeleafdemo.controller;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Locale;
 
 @Controller
 public class HelloWorldController {
     // controller method to show initial HTML form
-    @RequestMapping("/showForm")
+    @GetMapping("/showForm")
     public String showForm(){
         return "helloworld-form";
     }
@@ -33,6 +36,22 @@ public class HelloWorldController {
 
         //create the message
         String result = "Yo! "+ theName;
+
+        // add message to the model
+        model.addAttribute("message", result);
+
+        return "helloworld";
+    }
+
+    // need a controller method to read form data and add data to the model
+    @PostMapping("/processFormVersionThree")
+    public String processFormVersionThree(@RequestParam("studentName") String theName, Model model){
+
+        // convert all to caps
+        theName = theName.toUpperCase();
+
+        //create the message
+        String result = "Hey my Friend! "+ theName;
 
         // add message to the model
         model.addAttribute("message", result);
---
+++ 06-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/akashonlinehere/springboot/thymeleafdemo/model/Student.java
@@ -0,0 +1,25 @@
+package com.akashonlinehere.springboot.thymeleafdemo.model;
+
+public class Student {
+    private String firstName, lastName;
+
+    public Student(){
+
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
