a6c7577a97ffcefd7c174fd6f65bb3043a67ed40
Akash Shridharan <akashshridharan2802@gmail.com>
2024-10-24 11:55:19 +0530

    spring mvc thymeleaf

---
+++ 06-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/akashonlinehere/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -0,0 +1,19 @@
+package com.akashonlinehere.springboot.thymeleafdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+    // controller method to show initial HTML form
+    @RequestMapping("/showForm")
+    public String showForm(){
+        return "helloworld-form";
+    }
+
+    // need a controller to process form
+    @RequestMapping("/processForm")
+    public String processForm(){
+        return "helloworld";
+    }
+}
